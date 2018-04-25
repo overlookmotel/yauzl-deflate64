ca48a5251b0e1121f753756a6da9cc4a520e3048
overlookmotel <theoverlookmotel@gmail.com>
2018-04-25 01:38:03 +0100

    WIP3

--- foo.js
+++ foo.js
@@ -1,27 +1,58 @@
 'use strict';
 
+const fs = require('fs');
 const pathJoin = require('path').join;
 const yauzl = require('./lib/');
 
-const path = pathJoin(__dirname, '../yauzl-mac/test files/test.zip');
+const path = pathJoin(__dirname, 'test/test.zip');
+//const path = pathJoin(__dirname, 'unzipped with funzip.mov.zip');
+//const path = '/Cinebox/downloads/01660/unzipped/walkin%20the%20dog%20wip10b%20%28festival%20cut%29%201080%20dnxhd.zip';
+const pathOut = pathJoin(__dirname, 'out.mov');
 
 doIt((err, bytesRead) => {
-	if (err) throw err;
+	if (err) return console.log('ERROR:', err.stack);
+	console.log('DONE');
 	console.log('bytesRead:', bytesRead);
 });
 
 function doIt(cb) {
-	yauzl.open(path, {deflate64: true, autoClose: false, lazyEntries: true}, (err, zipFile) => {
+	yauzl.open(path, {funzip: true, autoClose: false, lazyEntries: true}, (err, zipFile) => {
 		if (err) return cb(err);
+
+		console.log('zipFile:', zipFile);
 
 		zipFile.on('error', cb);
 
 		zipFile.on('entry', entry => {
 			console.log('entry:', entry);
+			console.log('entry.compressionMethod:', entry.compressionMethod);
 
-			zipFile.openReadStream(entry, (err, stream) => {
+			process.exit();
+
+			zipFile.openReadStream(entry, {decompress: true}, (err, stream) => {
 				if (err) return cb(err);
 
+				const streamOut = fs.createWriteStream(pathOut);
+
+				let streamErr;
+				stream.on('error', err => {
+					console.log('yauzl stream error:', err);
+					streamErr = err;
+				});
+				streamOut.on('error', err => {
+					console.log('out stream error:', err);
+					streamErr = err;
+					cb(err);
+					stream.destroy();
+				});
+				streamOut.on('close', () => {
+					console.log('out stream close');
+					cb(streamErr);
+				});
+
+				stream.pipe(streamOut);
+
+				/*
 				let bytesRead = 0;
 				//const chunks = [];
 				stream.on('data', chunk => {
@@ -34,6 +65,7 @@ function doIt(cb) {
 					//console.log(data.toString());
 					cb(null, bytesRead);
 				});
+				*/
 			});
 		});
 
--- test/all.test.js
+++ test/all.test.js
@@ -8,6 +8,7 @@
 // Modules
 const chai = require('chai'),
 	{expect} = chai,
+	pathJoin = require('path').join,
 	yauzl = require('../lib/');
 
 // Init
@@ -18,8 +19,129 @@ chai.config.includeStack = true;
 /* jshint expr: true */
 /* global describe, it */
 
-describe('Tests', function() {
-	it.skip('all', function() {
-		expect(yauzl).to.be.ok;
+const PATH = pathJoin(__dirname, 'test.zip'),
+	BYTES = 220;
+
+it('Stream emits error if funzipPath invalid', function(cb) {
+	testError({
+		openOptions: {funzipPath: 'no-exist'},
+		expectFn: err => {
+			expect(err.code).to.equal('ENOENT');
+		}
+	}, cb);
+});
+
+it('Works normally', function(cb) {
+	testSuccess(null, cb);
+});
+
+it('Stream emits error if CRC32 wrong', function(cb) {
+	testError({
+		injectFn: entry => entry.crc32++,
+		expectFn: err => {
+			expect(err.message).to.equal('funzip exited with code 4 and stderr output \'funzip error: invalid compressed data--crc error\'');
+		}
+	}, cb);
+});
+
+describe('Stream emits error if uncompressed size', function() {
+	it('too big', function(cb) {
+		testError({
+			injectFn: entry => entry.uncompressedSize++,
+			expectFn: err => {
+				expect(err.message).to.equal('funzip exited with code 4 and stderr output \'funzip error: invalid compressed data--length error\'');
+			}
+		}, cb);
+	});
+
+	it('too small', function(cb) {
+		testError({
+			injectFn: entry => entry.uncompressedSize--,
+			expectFn: err => {
+				expect(err.message).to.equal('funzip exited with code 4 and stderr output \'funzip error: invalid compressed data--length error\'');
+			}
+		}, cb);
 	});
 });
+
+/*
+ * Helper functions
+ */
+function testSuccess(params, cb) {
+	const {openOptions, streamOptions, injectFn} = params || {};
+
+	getEntry(openOptions, (err, {zipFile, entry}) => {
+		if (err) return cb(err);
+
+		if (injectFn) injectFn(entry, zipFile);
+
+		getStream(zipFile, entry, streamOptions, (err, stream) => {
+			if (err) return cb(err);
+
+			let bytesRead = 0;
+			stream.on('data', chunk => bytesRead += chunk.length);
+
+			stream.on('end', () => {
+				expect(bytesRead).to.equal(BYTES);
+				cb();
+			});
+
+			stream.on('error', cb);
+		});
+	});
+}
+
+function testError(params, cb) {
+	const {openOptions, streamOptions, injectFn, expectFn} = params || {};
+
+	getEntry(openOptions, (err, {zipFile, entry}) => {
+		if (err) return cb(err);
+
+		if (injectFn) injectFn(entry, zipFile);
+
+		getStream(zipFile, entry, streamOptions, (err, stream) => {
+			if (err) return cb(err);
+
+			stream.on('data', () => {});
+
+			stream.on('error', err => {
+				expect(err).to.be.instanceof(Error);
+				if (expectFn) {
+					try {
+						expectFn(err, entry, zipFile);
+					} catch (err) {
+						return cb(err);
+					}
+				}
+
+				cb();
+			});
+
+			stream.on('end', () => cb(new Error('end event emitted')));
+		});
+	});
+}
+
+function getEntry(options, cb) {
+	options = Object.assign({autoClose: false, lazyEntries: true, funzip: true}, options);
+
+	yauzl.open(PATH, options, (err, zipFile) => {
+		if (err) return cb(err);
+
+		zipFile.on('error', cb);
+
+		zipFile.on('entry', entry => {
+			cb(null, {zipFile, entry});
+		});
+
+		zipFile.readEntry();
+	});
+}
+
+function getStream(zipFile, entry, options, cb) {
+	options = Object.assign({decompress: true}, options);
+	zipFile.openReadStream(entry, {decompress: true}, (err, stream) => {
+		if (err) return cb(err);
+		cb(null, stream);
+	});
+}
